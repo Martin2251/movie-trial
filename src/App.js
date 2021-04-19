feb168025af18a59521f315ec090484953b9dea8
martin lowe <martinlowe673@gmail.com>
2021-04-19 18:29:43 +0100

    added api

--- src/App.js
+++ src/App.js
@@ -86,6 +86,12 @@ const App = () => {
         "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg",
     },
   ]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=24885019";
+    const response = await fetch(url);
+    const responseJson = await response.json();
+  };
   return (
     <div className="movie-show">
       <MovieList movies={movies} />
