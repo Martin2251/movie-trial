0b376162009b41094aff510b9586a412dcdc1241
martin lowe <martinlowe673@gmail.com>
2021-04-20 07:41:29 +0100

    added search box

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 
 import "./App.css";
+import SearchBox from "./components/SearchBox";
 
-function App()
-  const [movies, setMovies] = useState(initalState);
-
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const getMovieRequest = async () => {
     const url = "http://www.omdbapi.com/?s=star wars&apikey=24885019";
+
     const response = await fetch(url);
     const responseJson = await response.json();
-
-    console.log(responseJson);
   };
   return (
     <div>
       <div>
-       <MovieListHeading heading="Movies"/>
-        <input value={inputValue} onChange={}></input>
-        <button>Search Movie</button>
+        <MovieListHeading heading="Movies" />
+        <SearchBox />
       </div>
 
       <div className="movie-show">
