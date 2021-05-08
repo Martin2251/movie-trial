b982c5890f04590e7e788b8f1d3d6aec34cac7aa
martin lowe <martinlowe673@gmail.com>
2021-05-08 11:07:42 +0100

    added contact page

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact page</h1>
+      <p>Welcome to my Contact page</p>
+    </div>
+  );
+}
+
+export default Contact;
