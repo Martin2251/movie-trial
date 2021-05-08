24017aa53b7e07b64066f354161f843bfeef9fe5
martin lowe <martinlowe673@gmail.com>
2021-05-08 11:13:27 +0100

    added nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App,css";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>Movies</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+export default Nav;
