a70d922183a29dac54697b7cf0cffdfc1ee37ce6
martin lowe <martinlowe673@gmail.com>
2021-05-09 18:51:16 +0100

    added MovieDisplay

--- src/App.js
+++ src/App.js
@@ -5,53 +5,14 @@ import "./App.css";
 import SearchBox from "./components/SearchBox";
 import Button from "./components/Button";
 import ClearButton from "./components/ClearButton";
-const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  // will trigger only ONCE when user lands on the page
-  useEffect(async () => {
-    const url = "http://www.omdbapi.com/?s=godfather&apikey=24885019";
-    const response = await fetch(url);
-    const responseJson = await response.json();
-    setMovies(responseJson.Search);
-  }, []);
-  const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=24885019`;
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MovieDisplay from "./MovieDisplay";
 
-    const response = await fetch(url);
-    const responseJson = await response.json();
-
-    // update the state
-    setMovies(responseJson.Search);
-    console.log(responseJson);
-    if (!responseJson.Error) {
-      // update the state
-      setMovies(responseJson.Search);
-    } else {
-      alert(responseJson.Error);
-    }
-  };
-  const resetAll = () => {
-    setMovies([]);
-    setSearchValue("");
-  };
+function App() {
   return (
-    <div>
-      <div className="search-cont">
-        <MovieListHeading heading=" Martin React Movie API" />
-        <SearchBox value={searchValue} onChange={setSearchValue} />
-        <Button onClickHandler={getMovieRequest} label="Search"></Button>
-        <div className="search-container">
-          <SearchBox value={searchValue} onChange={setSearchValue} />
-          <Button onClickHandler={getMovieRequest} label="Search"></Button>
-        </div>
-      </div>
-
-      <div className="movie-show">
-        <MovieList movies={movies} />
-      </div>
-      <ClearButton onClickHandler={resetAll} label="Clear All"></ClearButton>
-    </div>
+    <Router>
+      <MovieDisplay></MovieDisplay>
+    </Router>
   );
-};
+}
 export default App;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 //map over the list of movies with.map and return the poster
 
@@ -10,7 +10,9 @@ const MovieList = (props) => {
         <div className="image-container">
           <p>{movie.Title}</p>
           <img src={movie.Poster} alt="movie"></img>
-          <button className="Movie-button">View Movie</button>
+          <Link>
+            <button className="Movie-button">View Full Movie</button>
+          </Link>
         </div>
       ))}
     </>
