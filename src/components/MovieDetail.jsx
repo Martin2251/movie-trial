f9005d998f56bf9bdc8bb0867fc5bd39b931a43d
martin lowe <martinlowe673@gmail.com>
2021-05-10 07:54:14 +0100

    added further movie info

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -5,6 +5,13 @@ function MovieDetail() {
   useEffect(() => {}, []);
 
   const [movie, setMovie] = useState({});
+
+  const fetchMovie = async () => {
+    const fetchMovie = await fetch(
+      `http://www.omdbapi.com/?s=${searchValue}&apikey=24885019`
+    );
+    const movie = await fetchMovie.json();
+  };
   return (
     <div>
       <p>Movie</p>
