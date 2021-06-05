40b5a31ca156e9b15f4295a5dccf2b8ded068686
martin lowe <martinlowe673@gmail.com>
2021-06-05 15:42:12 +0100

    restyled searchbar

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const SearchBox = (props) => {
+  const styling = {
+    width: "20rem",
+    background: "#F2F1F9",
+    border: "none",
+    padding: "0.5rem",
+  };
   return (
     <div className="search-form">
       <input
-        className="search-bar"
+        style={styling}
         value={props.value}
         onChange={(event) => props.onChange(event.target.value)}
         placeholder="Type to search..."
